7578f2868cbd53c189f3f8839f0a99cfb0ac6eac
MaxPal <maxpaleev06@gmail.com>
2022-09-07 22:37:30 +0300

    Revert "V 1.6 Stable Mobile"
    This reverts commit eca229c6005c386f4a07b37d2b95eea6d8e5728d.

--- news/views.py
+++ news/views.py
@@ -16,8 +16,6 @@ class NewsDetailView(DetailView):
     context_object_name = 'articles'
 
 
-# @permission_required('news.change_articles')
-# @login_required
 class NewsUpdateViews(UpdateView):
     model = Articles
     template_name = 'news/create.html'
@@ -25,8 +23,6 @@ class NewsUpdateViews(UpdateView):
     form_class = ArticlesForm
 
 
-# @permission_required('news.delete_articles')
-# @login_required
 class NewsDeleteViews(DeleteView):
     model = Articles
     template_name = 'news/delete.html'
